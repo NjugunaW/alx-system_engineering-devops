1dbef05533af97d8251a6d70698bd399fa653697
NjugunaW <njugunawangui10@gmail.com>
2023-09-24 20:56:41 +0300

    0x0B. SSH Complete Project

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+#Using Puppet to make changes to our configuration file
+include stdlib
+
+_file_line { 'No password authentication':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+}
+
+_file_line { 'Use the private key':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school'
+}
