51329943d59856476a74d228ae06923a827edc8f
NjugunaW <njugunawangui10@gmail.com>
2023-09-24 12:05:01 -0700

    Task 4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,13 +3,13 @@
 # Using Puppet to make changes to our configuration file
 include stdlib
 
-_file_line { 'No password authentication':
+file_line { 'No password authentication':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
 }
 
-_file_line { 'Use the private key':
+file_line { 'Use the private key':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/school'
